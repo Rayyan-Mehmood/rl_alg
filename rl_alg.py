26ce0abad5a041943484805414905380ad088ed5
Rayyan-Mehmood <rayyan.mehmood@gmail.com>
2024-01-27 14:01:16 +0000

    Put code for 'when enough prbs available' into its own function

--- rl_alg.py
+++ rl_alg.py
@@ -6,6 +6,7 @@ from gymnasium.spaces import Discrete, Box, MultiDiscrete
 import pandas as pd
 import matplotlib.pyplot as plot
 
+num_slices = 2
 max_prbs_inf = 14
 max_req_prbs = 2 # max total prbs a slice can require
 min_req_prbs = 0
@@ -28,8 +29,18 @@ class RicEnv(Env):
         # req_prbs should oscillate around max_prbs_req/2
         self.sine_amplitude = np.rint((max_req_prbs/2) * np.sin(sine_time_range) + max_req_prbs/2)
         self.time = 0
-        self.state = np.array([1, self.sine_amplitude[self.time], self.sine_amplitude[self.time], min_curr_prbs, min_curr_prbs])
+        self.state = np.array([8, self.sine_amplitude[self.time], self.sine_amplitude[self.time], min_curr_prbs, min_curr_prbs])
         # remember: it's required prbs, not the amount that's there
+
+    def calc_individual_reward(self, r, a):
+        if a == r:
+            individual_reward = 120
+        elif a > r:
+            individual_reward = 100 - (10 * (a - r))
+        else:
+            individual_reward = -100 + (10 * (r - a))
+
+        return individual_reward
 
     def step(self, action):
         # save the current state
@@ -59,43 +70,34 @@ class RicEnv(Env):
                 reward = 100
             else:
                 reward = - abs(action[0] - ideal_action_0) - abs(action[1] - ideal_action_1)
-        # if enough pRBs available and slice gets the num prbs it requires, then reward
-        # Case 1
-        elif new_curr_prbs_s1 == req_prbs_s1 and new_curr_prbs_s2 == req_prbs_s2:
-            reward = 100
-        # if enough pRBs available and slice is allocated more prbs than required
-        # Case 2, 3, 4
-        elif new_curr_prbs_s1 >= req_prbs_s1 and new_curr_prbs_s2 >= req_prbs_s2:
-            reward = 100 - 5 * ((new_curr_prbs_s1-req_prbs_s1)+(new_curr_prbs_s2-req_prbs_s2))
-        # if enough pRBs available and both slices are allocated less prbs than required
-        # Case 5, 6, 8
-        elif new_curr_prbs_s1 <= req_prbs_s1 and new_curr_prbs_s2 <= req_prbs_s2:
-            reward = 5 * (- (req_prbs_s1/(new_curr_prbs_s1+1.0001)) - (req_prbs_s2/(new_curr_prbs_s2+1.0001)))
-        # if enough pRBs available and one slice is allocated more and one slice is allocated less
-        # Case 7
         else:
-            reward = 50 - (5 * (abs(new_curr_prbs_s1-req_prbs_s1)+abs(new_curr_prbs_s2-req_prbs_s2)))
+            total_reward = 0
+            individual_reward = 0
+            for slice in range(1, num_slices+1):
+                individual_reward = self.calc_individual_reward(prev_state[slice], self.state[slice+2])
+                total_reward = total_reward + individual_reward
+            reward = total_reward
 
 
         if new_prbs_inf < 0:
-            reward = -100
+            reward = -1000
             self.state[0] = 0
         elif new_prbs_inf > max_prbs_inf:
-            reward = -100
+            reward = -1000
             self.state[0] = max_prbs_inf
 
         if new_curr_prbs_s1 < 0:
-            reward = -100
+            reward = -1000
             self.state[3] = 0
         elif new_curr_prbs_s1 > max_curr_prbs:
-            reward = -100
+            reward = -1000
             self.state[3] = max_curr_prbs
 
         if new_curr_prbs_s2 < 0:
-            reward = -100
+            reward = -1000
             self.state[4] = 0
         elif new_curr_prbs_s2 > max_curr_prbs:
-            reward = -100
+            reward = -1000
             self.state[4] = max_curr_prbs
 
 
@@ -125,12 +127,12 @@ q_values = np.zeros([np.size(prbs_inf_states)*np.size(prbs_req_s1_states)*np.siz
 q_table = pd.DataFrame(q_values, columns=col_indices, index=row_indices)
 
 # Hyperparameters
-alpha = 0.7
-gamma = 0.0001
+alpha = 0.99999
+gamma = 0.00001
 epsilon = 0.9999
 
 # Training
-for i in range(1, 100000):
+for i in range(1, 200000):
     print(i)
     # select action (explore vs exploit)
     if random.uniform(0, 1) < epsilon:
@@ -154,7 +156,7 @@ q_table.to_csv('q_table.csv')
 # Testing
 # initializing the environment
 env.time = 0
-env.state = np.array([1, env.sine_amplitude[env.time], env.sine_amplitude[env.time], min_curr_prbs, min_curr_prbs])
+env.state = np.array([8, env.sine_amplitude[env.time], env.sine_amplitude[env.time], min_curr_prbs, min_curr_prbs])
 print("pRBs_inf \t pRBs_req \t pRBs_s1 \t pRBs_s2 \t A1 \t A2")
 for i in range(0, 20): # upper bound is exclusive
     # select action (exploit)
