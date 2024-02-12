b4665d076f253b7d70fd0931a8bb345d51167367
Rayyan-Mehmood <rayyan.mehmood@gmail.com>
2024-02-12 10:08:27 +0000

    Added comments and reset the reward function

--- dql_alg.py
+++ dql_alg.py
@@ -6,9 +6,9 @@ import tensorflow as tf
 import keras
 from collections import deque
 
-prbs_inf_init = 4
+prbs_inf_init = 8
 num_slices = 2
-max_prbs_inf = 8
+max_prbs_inf = 14
 max_req_prbs = 2 # max total prbs a slice can require
 min_req_prbs = 0
 max_curr_prbs = max_req_prbs
@@ -24,20 +24,26 @@ class RicEnv:
         action2_space = np.arange(min_allocate, max_allocate + 1)
         self.action_space_MI = pd.MultiIndex.from_product([action1_space, action2_space])
         self.action_space = pd.DataFrame(index=self.action_space_MI, columns=['Value'])
-        # state = [prbs_inf, req_prbs_s1, req_prbs_s2, curr_prbs_s1, curr_prbs_s2]
 
-        self.prbs_req_data = np.genfromtxt('data_sin.csv', delimiter=',')
+        # state = [prbs_inf, req_prbs_s1, req_prbs_s2, curr_prbs_s1, curr_prbs_s2]
+        # self.prbs_req_data = np.genfromtxt('data_sin.csv', delimiter=',') # loading from a csv file
+        # Creating function of req_prbs
+        sine_time_range = np.arange(0, 10, 0.5) # generating manually
+        self.prbs_req_data = np.rint((max_req_prbs / 2) * np.sin(sine_time_range) + max_req_prbs / 2)
         self.time = 0
         self.state = np.array([prbs_inf_init, self.prbs_req_data[self.time], self.prbs_req_data[self.time], min_curr_prbs, min_curr_prbs])
-        # remember: it's required prbs, not the amount that's there
 
+    # Calculates the individual reward for each slice based on the slice requirements and the amount of pRBs it
+    # currently has
     def calc_individual_reward(self, r, a):
+        # r = number of pRBs slice requires
+        # a = number of pRBs slice currently has
         if a == r:
-            individual_reward = 150
+            individual_reward = 120
         elif a > r:
             individual_reward = 100 - (10 * (a - r))
         else:
-            individual_reward = 50 - (10 * (r - a))
+            individual_reward = -100 + (10 * (r - a))
 
         return individual_reward
 
@@ -54,12 +60,15 @@ class RicEnv:
         new_curr_prbs_s1 = curr_prbs_s1 + action[0]
         new_curr_prbs_s2 = curr_prbs_s2 + action[1]
         self.time = self.time + 1  # update time
-        if self.time > 19:
-            self.time = 0 # reset time
+        if self.time > 19:  # 19 is the length of one episode
+            self.time = 0  # reset time
         self.state = np.array([new_prbs_inf, self.prbs_req_data[self.time], self.prbs_req_data[self.time], new_curr_prbs_s1, new_curr_prbs_s2])
 
-        # calculate reward
-        # if not enough pRBs available
+        # Calculate Reward
+
+        # If not enough pRBs available, calculate the ideal action and then update the required pRBs for each slice
+        # for example, if there are 2 pRBs available and each slice has 0 pRBs and requires 2 pRBs,
+        # the ideal action would be to allocate 1 to each slice. Thus, the required pRBs for each slice will become 1.
         if prbs_inf - (req_prbs_s1 - curr_prbs_s1) - (req_prbs_s2 - curr_prbs_s2) < 0:
             difference_s1 = req_prbs_s1 - curr_prbs_s1
             difference_s2 = req_prbs_s2 - curr_prbs_s2
@@ -68,6 +77,7 @@ class RicEnv:
             prev_state[1] = round(prev_state[3] + ideal_action_0)
             prev_state[2] = round(prev_state[4] + ideal_action_1)
 
+        # Calculate the individual reward for each slice and then sum the rewards
         total_reward = 0
         individual_reward = 0
         for slice in range(1, num_slices+1):
@@ -75,10 +85,10 @@ class RicEnv:
             total_reward = total_reward + individual_reward
         reward = total_reward
 
-        # Impossible actions are punished heavily
+        # Impossible actions (which lead to prbs_inf being negative for e.g.) are punished heavily
         if new_prbs_inf < 0:
             reward = -150
-            self.state[0] = 0
+            self.state[0] = 0  # set prbs_inf to zero
         elif new_prbs_inf > max_prbs_inf:
             reward = -150
             self.state[0] = max_prbs_inf
@@ -111,13 +121,15 @@ class RicAgent:
         The highest value 0.7 is the Q-Value.
         The index of the highest action (0.7) is action #1.
         """
-        learning_rate = 0.01
+        learning_rate = 0.001
+        optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+
         model = keras.Sequential()
         model.add(keras.layers.Input(shape=state_shape))
         model.add(keras.layers.Dense(32, activation='relu'))
         model.add(keras.layers.Dense(32, activation='relu'))
         model.add(keras.layers.Dense(num_actions, activation='linear'))
-        model.compile(optimizer='adam', loss='mse')
+        model.compile(optimizer=optimizer, loss='mse')
         # model.summary()
         return model
 
@@ -126,7 +138,7 @@ class RicAgent:
         if len(replay_memory) < MIN_REPLAY_SIZE:
             return
 
-        batch_size = 32
+        batch_size = 64
         mini_batch = random.sample(replay_memory, batch_size)
 
         # Collect observations from the mini-batch
@@ -155,10 +167,10 @@ def main():
     replay_memory = deque(maxlen=50_000)
 
     # Hyper-parameters
-    epsilon = 0.9999
+    epsilon = 0.9999  # want to explore all the time
 
     # Training
-    for i in range(1, 2999):
+    for i in range(1, 5000):
         print(i)
         # select action (explore vs exploit)
         if random.uniform(0, 1) < epsilon:
@@ -184,14 +196,10 @@ def main():
     env.state = np.array([prbs_inf_init, env.prbs_req_data[env.time], env.prbs_req_data[env.time], min_curr_prbs, min_curr_prbs])
     state = tuple(env.state)
     print("pRBs_inf \t pRBs_req \t pRBs_s1 \t pRBs_s2 \t A1 \t A2")
-    # prbs_s1_record = np.zeros(20)
-    # prbs_s2_record = np.zeros(20)
     for i in range(0, 20): # upper bound is exclusive
-        # prbs_s1_record[env.time] = env.state[3]
-        # prbs_s2_record[env.time] = env.state[4]
         # select action (exploit)
         predicted = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
-        print(np.reshape(np.round(predicted, 3), (5, 5)))
+        # print(np.reshape(np.round(predicted, 3), (5, 5)))
         action = env.action_space_MI[np.argmax(predicted)]
         print("{} \t \t {} \t \t {} \t \t {} \t \t {}  \t {}".format(env.state[0],
                                                                                  env.prbs_req_data[env.time], env.state[3],
