30411afce2dd30e9ef796b71ce33dbfddb1bac42
Rayyan-Mehmood <rayyan.mehmood@gmail.com>
2024-03-05 15:37:08 +0000

    Added third slice (for DQL). Trained on 2 hardcoded states

--- dql_alg.py
+++ dql_alg.py
@@ -7,10 +7,10 @@ import keras
 from keras.models import load_model
 from collections import deque
 
+num_slices = 3
 prbs_inf_init = 8
-num_slices = 2
 max_prbs_inf = 14
-max_req_prbs = 2 # max total prbs a slice can require
+max_req_prbs = 2  # max total prbs a slice can require
 min_req_prbs = 0
 max_curr_prbs = max_req_prbs
 min_curr_prbs = 0
@@ -23,16 +23,19 @@ class RicEnv:
         # Encoding the action space as a Multi-Index array of tuples
         action1_space = np.arange(min_allocate, max_allocate + 1)
         action2_space = np.arange(min_allocate, max_allocate + 1)
-        self.action_space_MI = pd.MultiIndex.from_product([action1_space, action2_space])
+        action3_space = np.arange(min_allocate, max_allocate + 1)
+        self.action_space_MI = pd.MultiIndex.from_product([action1_space, action2_space, action3_space])
         self.action_space = pd.DataFrame(index=self.action_space_MI, columns=['Value'])
 
         # state = [prbs_inf, req_prbs_s1, req_prbs_s2, curr_prbs_s1, curr_prbs_s2]
         # self.prbs_req_data = np.genfromtxt('data_sin.csv', delimiter=',') # loading from a csv file
         # Creating function of req_prbs
         sine_time_range = np.arange(0, 10, 0.5) # generating manually
-        self.prbs_req_data = np.rint((max_req_prbs / 2) * np.sin(sine_time_range) + max_req_prbs / 2)
+        self.prbs_req_data = np.rint((max_req_prbs / 2) * np.sin(sine_time_range) + max_req_prbs / 2)  # already changed to cosine
         self.time = 0
-        self.state = np.array([prbs_inf_init, self.prbs_req_data[self.time], self.prbs_req_data[self.time], min_curr_prbs, min_curr_prbs])
+        self.episode_length = 20
+        self.state = np.array([prbs_inf_init, self.prbs_req_data[self.time], self.prbs_req_data[self.time],
+                               self.prbs_req_data[self.time], min_curr_prbs, min_curr_prbs, min_curr_prbs])
 
     # Calculates the individual reward for each slice based on the slice requirements and the amount of pRBs it
     # currently has
@@ -44,7 +47,7 @@ class RicEnv:
         elif a > r:
             individual_reward = 100 - (10 * (a - r))
         else:
-            individual_reward = -100 - (10 * (r - a))
+            individual_reward = -100 - (10 * (r - abs(a)))
 
         return individual_reward
 
@@ -54,35 +57,76 @@ class RicEnv:
         prbs_inf = prev_state[0]
         req_prbs_s1 = prev_state[1]
         req_prbs_s2 = prev_state[2]
-        curr_prbs_s1 = prev_state[3]
-        curr_prbs_s2 = prev_state[4]
+        req_prbs_s3 = prev_state[3]
+        curr_prbs_s1 = prev_state[4]
+        curr_prbs_s2 = prev_state[5]
+        curr_prbs_s3 = prev_state[6]
         # update the state
-        new_prbs_inf = self.state[0] - action[0] - action[1]
+        new_prbs_inf = self.state[0] - action[0] - action[1] - action[2]
         new_curr_prbs_s1 = curr_prbs_s1 + action[0]
         new_curr_prbs_s2 = curr_prbs_s2 + action[1]
+        new_curr_prbs_s3 = curr_prbs_s3 + action[2]
         self.time = self.time + 1  # update time
-        if self.time > 19:  # 19 is the length of one episode
+        if self.time >= self.episode_length:
             self.time = 0  # reset time
-        self.state = np.array([new_prbs_inf, self.prbs_req_data[self.time], self.prbs_req_data[self.time], new_curr_prbs_s1, new_curr_prbs_s2])
+        self.state = np.array([new_prbs_inf, self.prbs_req_data[self.time], self.prbs_req_data[self.time],
+                               self.prbs_req_data[self.time], new_curr_prbs_s1, new_curr_prbs_s2, new_curr_prbs_s3])
 
         # Calculate Reward
 
         # If not enough pRBs available, calculate the ideal action and then update the required pRBs for each slice
         # for example, if there are 2 pRBs available and each slice has 0 pRBs and requires 2 pRBs,
         # the ideal action would be to allocate 1 to each slice. Thus, the required pRBs for each slice will become 1.
-        if prbs_inf - (req_prbs_s1 - curr_prbs_s1) - (req_prbs_s2 - curr_prbs_s2) < 0:
+        if prbs_inf - (req_prbs_s1 - curr_prbs_s1) - (req_prbs_s2 - curr_prbs_s2) - (req_prbs_s3 - curr_prbs_s3) < 0:
             difference_s1 = req_prbs_s1 - curr_prbs_s1
             difference_s2 = req_prbs_s2 - curr_prbs_s2
-            ideal_action_0 = round((difference_s1/(difference_s1+difference_s2-0.00001)) * prbs_inf)
-            ideal_action_1 = round((difference_s2/(difference_s1+difference_s2+0.00001)) * prbs_inf)
-            prev_state[1] = round(prev_state[3] + ideal_action_0)
-            prev_state[2] = round(prev_state[4] + ideal_action_1)
+            difference_s3 = req_prbs_s3 - curr_prbs_s3
+            # calculating the optimal actions
+            if difference_s1 < 0 and difference_s2 > 0 and difference_s3 > 0:
+                prbs_inf = prbs_inf - difference_s1
+                oa1 = difference_s1
+                oa2 = round((difference_s2 / (difference_s2 + difference_s3 - 0.00001)) * prbs_inf)
+                oa3 = round((difference_s3 / (difference_s2 + difference_s3 + 0.00001)) * prbs_inf)
+            elif difference_s1 > 0 and difference_s2 < 0 and difference_s3 > 0:
+                prbs_inf = prbs_inf - difference_s2
+                oa2 = difference_s2
+                oa1 = round((difference_s1 / (difference_s1 + difference_s3 - 0.00001)) * prbs_inf)
+                oa3 = round((difference_s3 / (difference_s1 + difference_s3 + 0.00001)) * prbs_inf)
+            elif difference_s1 > 0 and difference_s2 > 0 and difference_s3 < 0:
+                prbs_inf = prbs_inf - difference_s3
+                oa3 = difference_s3
+                oa1 = round((difference_s1 / (difference_s1 + difference_s2 - 0.00001)) * prbs_inf)
+                oa2 = round((difference_s2 / (difference_s1 + difference_s2 + 0.00001)) * prbs_inf)
+            elif difference_s1 < 0 and difference_s2 < 0 and difference_s3 > 0:
+                prbs_inf = prbs_inf - difference_s1 - difference_s2
+                oa1 = difference_s1
+                oa2 = difference_s2
+                oa3 = prbs_inf
+            elif difference_s1 < 0 and difference_s2 > 0 and difference_s3 < 0:
+                prbs_inf = prbs_inf - difference_s1 - difference_s3
+                oa1 = difference_s1
+                oa2 = prbs_inf
+                oa3 = difference_s3
+            elif difference_s1 > 0 and difference_s2 < 0 and difference_s3 < 0:
+                prbs_inf = prbs_inf - difference_s2 - difference_s3
+                oa1 = prbs_inf
+                oa2 = difference_s2
+                oa3 = difference_s3
+            else:
+                # all differences are positive
+                oa1 = round((difference_s1 / (difference_s1 + difference_s2 + difference_s3)) * prbs_inf)
+                oa2 = round((difference_s2 / (difference_s1 + difference_s2 + difference_s3)) * prbs_inf)
+                oa3 = prbs_inf - oa1 - oa2  # allocate whatever is left
+
+            prev_state[1] = round(prev_state[4] + oa1)
+            prev_state[2] = round(prev_state[5] + oa2)
+            prev_state[3] = round(prev_state[6] + oa3)
 
         # Calculate the individual reward for each slice and then sum the rewards
         total_reward = 0
         individual_reward = 0
-        for slice in range(1, num_slices+1):
-            individual_reward = self.calc_individual_reward(prev_state[slice], self.state[slice+2])
+        for slice in range(1, num_slices + 1):
+            individual_reward = self.calc_individual_reward(prev_state[slice], self.state[slice + num_slices])
             total_reward = total_reward + individual_reward
         reward = total_reward
 
@@ -91,23 +135,30 @@ class RicEnv:
             reward = -150
             self.state[0] = 0  # set prbs_inf to zero
         elif new_prbs_inf > max_prbs_inf:
-            reward = -150
             self.state[0] = max_prbs_inf
 
         if new_curr_prbs_s1 < 0:
             reward = -150
-            self.state[3] = 0
+            self.state[4] = 0
         elif new_curr_prbs_s1 > max_curr_prbs:
-            reward = -150
-            self.state[3] = max_curr_prbs
+            self.state[4] = max_curr_prbs
 
         if new_curr_prbs_s2 < 0:
             reward = -150
-            self.state[4] = 0
+            self.state[5] = 0
         elif new_curr_prbs_s2 > max_curr_prbs:
-            reward = -150
-            self.state[4] = max_curr_prbs
+            self.state[5] = max_curr_prbs
 
+        if new_curr_prbs_s3 < 0:
+            reward = -150
+            self.state[6] = 0
+        elif new_curr_prbs_s3 > max_curr_prbs:
+            self.state[6] = max_curr_prbs
+
+        if self.time == 0:
+            self.state[0] = prbs_inf_init
+            self.state[3] = min_curr_prbs
+            self.state[4] = min_curr_prbs
 
         info = {}
         done = False
@@ -161,65 +212,42 @@ class RicAgent:
 def test_other_inputs(env, main_model):
     # Testing other inputs
     print("TESTING OTHER INPUTS")
-    env.state = np.array([3, 1, 1, 0, 0])
+    env.state = np.array([8, 2, 2, 2, 0, 0, 0])
     print("State: ", env.state)
-    predicted = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
-    print("Q-values: ")
-    print(np.reshape(np.round(predicted, 3), (5, 5)))
+    predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()
+    action = env.action_space_MI[np.argmax(predictions)]
+    print("Action: ")
+    print(action)
 
-    env.state = np.array([3, 1, 1, 0, 1])
+    env.state = np.array([4, 0, 0, 0, 2, 2, 2])
     print("State: ", env.state)
-    predicted = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
-    print("Q-values: ")
-    print(np.reshape(np.round(predicted, 3), (5, 5)))
-
-    env.state = np.array([2, 1, 1, 0, 0])
-    print("State: ", env.state)
-    predicted = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
-    print("Q-values: ")
-    print(np.reshape(np.round(predicted, 3), (5, 5)))
-
-    env.state = np.array([5, 0, 0, 1, 1])
-    print("State: ", env.state)
-    predicted = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
-    print("Q-values: ")
-    print(np.reshape(np.round(predicted, 3), (5, 5)))
-
-    env.state = np.array([4, 0, 0, 2, 2])
-    print("State: ", env.state)
-    predicted = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
-    print("Q-values: ")
-    print(np.reshape(np.round(predicted, 3), (5, 5)))
-
-    env.state = np.array([8, 2, 2, 0, 0])
-    print("State: ", env.state)
-    predicted = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
-    print("Q-values: ")
-    print(np.reshape(np.round(predicted, 3), (5, 5)))
+    predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()
+    action = env.action_space_MI[np.argmax(predictions)]
+    print("Action: ")
+    print(action)
 
 
 def test(env, main_model):
     # Testing
     # initializing the environment
     env.time = 0
-    env.state = np.array(
-        [prbs_inf_init, env.prbs_req_data[env.time], env.prbs_req_data[env.time], min_curr_prbs, min_curr_prbs])
+    env.state = np.array([prbs_inf_init, env.prbs_req_data[env.time], env.prbs_req_data[env.time],
+                               env.prbs_req_data[env.time], min_curr_prbs, min_curr_prbs, min_curr_prbs])
 
-    print("pRBs_inf \t pRBs_req \t pRBs_s1 \t pRBs_s2 \t A1 \t A2")
-    for i in range(0, 20):  # upper bound is exclusive
+    print("pRBs_inf \t req_s1 \t req_s2 \t req_s3 \t pRBs_s1 \t pRBs_s2 \t pRBs_s3 \t A1 \t A2 \t A3")
+    for i in range(0, env.episode_length):  # upper bound is exclusive
         # select action (exploit)
         predicted = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
-        print(np.reshape(np.round(predicted, 3), (5, 5)))
+        # print(np.reshape(np.round(predicted, 3), (5, 5)))
         action = env.action_space_MI[np.argmax(predicted)]
-        print("{} \t \t {} \t \t {} \t \t {} \t \t {}  \t {}".format(env.state[0],
-                                                                     env.prbs_req_data[env.time], env.state[3],
-                                                                     env.state[4],
-                                                                     action[0], action[1]))
+        print("{} \t \t \t {} \t \t {} \t \t {} \t \t {} \t \t {} \t \t {} \t \t {}  \t {}  \t {}"
+              .format(env.state[0], env.state[1], env.state[2], env.state[3], env.state[4],
+                      env.state[5], env.state[6], action[0], action[1], action[2]))
         # move 1 step forward
         next_state, reward, done, truncated, info = env.step(action)
 
     # Test other inputs and print out the results
-    test_other_inputs(env, main_model)
+    # test_other_inputs(env, main_model)
 
 def main():
 
@@ -228,32 +256,44 @@ def main():
     agent = RicAgent()
 
     # main_model = agent.build_net(env.state.shape, len(env.action_space))
-    main_model = load_model("test_21_3.h5")
+    main_model = load_model("test_23_3.h5")
     replay_memory = deque(maxlen=50_000)
 
     # Hyper-parameters
     epsilon = 0.7  # want to explore all the time
 
     # Training
-    for i in range(1, 50000):
+    for i in range(1, 5000):
         print(i)
         # select action (explore vs exploit)
-        if random.uniform(0, 1) < epsilon:
-            action = env.action_space.sample().index[0]  # Explore action space
-            action_index = env.action_space_MI.get_loc(action)
-        else:
-            predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
-            action = env.action_space_MI[np.argmax(predictions)]
-            action_index = env.action_space_MI.get_loc(action)
-
         # if random.uniform(0, 1) < epsilon:
-        #     action = (2, 2)
-        #     action_index = 24
-        #     env.state = np.array([8, 2, 2, 0, 0])
+        #     action = env.action_space.sample().index[0]  # Explore action space
+        #     action_index = env.action_space_MI.get_loc(action)
         # else:
-        #     action = (-2, -2)
-        #     action_index = 0
-        #     env.state = np.array([4, 0, 0, 2, 2])
+        #     predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
+        #     action = env.action_space_MI[np.argmax(predictions)]
+        #     action_index = env.action_space_MI.get_loc(action)
+
+        if random.uniform(0, 1) < 0.5:
+            env.state = np.array([8, 2, 2, 2, 0, 0, 0])
+            state = tuple(env.state)
+            if random.uniform(0, 1) < epsilon:
+                action = env.action_space.sample().index[0]  # Explore action space
+                action_index = env.action_space_MI.get_loc(action)
+            else:
+                predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
+                action = env.action_space_MI[np.argmax(predictions)]
+                action_index = env.action_space_MI.get_loc(action)
+        else:
+            env.state = np.array([4, 0, 0, 0, 2, 2, 2])
+            state = tuple(env.state)
+            if random.uniform(0, 1) < epsilon:
+                action = env.action_space.sample().index[0]  # Explore action space
+                action_index = env.action_space_MI.get_loc(action)
+            else:
+                predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
+                action = env.action_space_MI[np.argmax(predictions)]
+                action_index = env.action_space_MI.get_loc(action)
 
         # move 1 step forward
         next_state, reward, done, truncated, info = env.step(action)
@@ -265,9 +305,10 @@ def main():
         state = next_state
 
     # Save model
-    main_model.save("test_21_4.h5")
+    main_model.save("test_23_3.h5")
     # Testing
-    test(env, main_model)
+    # test(env, main_model)
+    test_other_inputs(env, main_model)
 
 
 if __name__ == "__main__":
