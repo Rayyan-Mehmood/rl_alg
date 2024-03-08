a9284bf5e9ced77c5d3c2ced4c847859de729629
Rayyan-Mehmood <rayyan.mehmood@gmail.com>
2024-03-08 18:14:45 +0000

    Added test metrics

--- dql_alg.py
+++ dql_alg.py
@@ -8,15 +8,21 @@ from keras.models import load_model
 from collections import deque
 
 num_slices = 3
-prbs_inf_init = 8
-max_prbs_inf = 14
-max_req_prbs = 2  # max total prbs a slice can require
+prbs_inf_init = 50
+max_prbs_inf = prbs_inf_init
+max_req_prbs = 10  # max total prbs a slice can require
 min_req_prbs = 0
-max_curr_prbs = max_req_prbs
+max_curr_prbs = 35
 min_curr_prbs = 0
-max_allocate = max_req_prbs
-min_allocate = -max_req_prbs
-
+max_allocate = 35
+min_allocate = -35
+num_iterations = 0
+testing_iterations = 12
+epsilon = 0.5
+impossible_action_reward = -150
+show_plots = False
+# initial_epsilon = 0.7
+# final_epsilon = 0.4
 
 class RicEnv:
     def __init__(self):
@@ -27,15 +33,21 @@ class RicEnv:
         self.action_space_MI = pd.MultiIndex.from_product([action1_space, action2_space, action3_space])
         self.action_space = pd.DataFrame(index=self.action_space_MI, columns=['Value'])
 
-        # state = [prbs_inf, req_prbs_s1, req_prbs_s2, curr_prbs_s1, curr_prbs_s2]
-        # self.prbs_req_data = np.genfromtxt('data_sin.csv', delimiter=',') # loading from a csv file
+        excel_file_path = 'data/data_real_10_slices.xlsx'
+        data_frame = pd.read_excel(excel_file_path, skiprows=1, header=None)
+        prbs_req_data_s1 = data_frame[2].values  # row C
+        prbs_req_data_s2 = data_frame[3].values  # row D
+        prbs_req_data_s3 = data_frame[6].values  # row D
+        self.prbs_req_s1 = np.round(np.array(prbs_req_data_s1)).astype(int)
+        self.prbs_req_s2 = np.round(np.array(prbs_req_data_s2)).astype(int)
+        self.prbs_req_s3 = np.round(np.array(prbs_req_data_s3)).astype(int)
         # Creating function of req_prbs
-        sine_time_range = np.arange(0, 10, 0.5) # generating manually
-        self.prbs_req_data = np.rint((max_req_prbs / 2) * np.sin(sine_time_range) + max_req_prbs / 2)  # already changed to cosine
+        # sine_time_range = np.arange(0, 10, 0.5) # generating manually
+        # self.prbs_req_data = np.rint((max_req_prbs / 2) * np.sin(sine_time_range) + max_req_prbs / 2)  # already changed to cosine
         self.time = 0
         self.episode_length = 20
-        self.state = np.array([prbs_inf_init, self.prbs_req_data[self.time], self.prbs_req_data[self.time],
-                               self.prbs_req_data[self.time], min_curr_prbs, min_curr_prbs, min_curr_prbs])
+        self.state = np.array([prbs_inf_init, random.randint(0, max_req_prbs), random.randint(0, max_req_prbs),
+                               random.randint(0, max_req_prbs), min_curr_prbs, min_curr_prbs, min_curr_prbs])
 
     # Calculates the individual reward for each slice based on the slice requirements and the amount of pRBs it
     # currently has
@@ -51,7 +63,7 @@ class RicEnv:
 
         return individual_reward
 
-    def step(self, action):
+    def step(self, action, testing=False):
         # save the current state
         prev_state = list(self.state)
         prbs_inf = prev_state[0]
@@ -69,8 +81,13 @@ class RicEnv:
         self.time = self.time + 1  # update time
         if self.time >= self.episode_length:
             self.time = 0  # reset time
-        self.state = np.array([new_prbs_inf, self.prbs_req_data[self.time], self.prbs_req_data[self.time],
-                               self.prbs_req_data[self.time], new_curr_prbs_s1, new_curr_prbs_s2, new_curr_prbs_s3])
+        if not testing:
+            self.state = np.array([new_prbs_inf, random.randint(0, max_req_prbs), random.randint(0, max_req_prbs),
+                               random.randint(0, max_req_prbs), new_curr_prbs_s1, new_curr_prbs_s2, new_curr_prbs_s3])
+        else:
+            self.state = np.array([new_prbs_inf, self.prbs_req_s1[self.time], self.prbs_req_s2[self.time],
+                               self.prbs_req_s3[self.time], new_curr_prbs_s1, new_curr_prbs_s2,
+                                   new_curr_prbs_s3])
 
         # Calculate Reward
 
@@ -132,39 +149,39 @@ class RicEnv:
 
         # Impossible actions (which lead to prbs_inf being negative for e.g.) are punished heavily
         if new_prbs_inf < 0:
-            reward = -150
+            reward = impossible_action_reward
             self.state[0] = 0  # set prbs_inf to zero
         elif new_prbs_inf > max_prbs_inf:
             self.state[0] = max_prbs_inf
 
         if new_curr_prbs_s1 < 0:
-            reward = -150
+            reward = impossible_action_reward
             self.state[4] = 0
         elif new_curr_prbs_s1 > max_curr_prbs:
             self.state[4] = max_curr_prbs
 
         if new_curr_prbs_s2 < 0:
-            reward = -150
+            reward = impossible_action_reward
             self.state[5] = 0
         elif new_curr_prbs_s2 > max_curr_prbs:
             self.state[5] = max_curr_prbs
 
         if new_curr_prbs_s3 < 0:
-            reward = -150
+            reward = impossible_action_reward
             self.state[6] = 0
         elif new_curr_prbs_s3 > max_curr_prbs:
             self.state[6] = max_curr_prbs
 
         if self.time == 0:
             self.state[0] = prbs_inf_init
-            self.state[3] = min_curr_prbs
             self.state[4] = min_curr_prbs
+            self.state[5] = min_curr_prbs
+            self.state[6] = min_curr_prbs
 
         info = {}
         done = False
         truncated = False
         return tuple(self.state), reward, done, truncated, info
-
 
 class RicAgent:
     def build_net(self, state_shape, num_actions):
@@ -210,44 +227,116 @@ class RicAgent:
 
 
 def test_other_inputs(env, main_model):
+    def print_testcase(env, main_model, state):
+        env.state = state
+        print("State: ", env.state)
+        predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()
+        action = env.action_space_MI[np.argmax(predictions)]
+        q_value = np.max(predictions)
+        print("Action: ", action)
+        print("Q-value: ", q_value)
+
     # Testing other inputs
     print("TESTING OTHER INPUTS")
-    env.state = np.array([8, 2, 2, 2, 0, 0, 0])
-    print("State: ", env.state)
-    predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()
-    action = env.action_space_MI[np.argmax(predictions)]
-    print("Action: ")
-    print(action)
 
-    env.state = np.array([4, 0, 0, 0, 2, 2, 2])
-    print("State: ", env.state)
-    predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()
-    action = env.action_space_MI[np.argmax(predictions)]
-    print("Action: ")
-    print(action)
+    print_testcase(env, main_model, np.array([50, 2, 2, 2, 0, 0, 0]))
+    print_testcase(env, main_model, np.array([40, 0, 0, 0, 2, 6, 2]))
+    print_testcase(env, main_model, np.array([48, 0, 9, 7, 0, 0, 2]))
+    print_testcase(env, main_model, np.array([40, 1, 6, 2, 6, 2, 2]))
+    print_testcase(env, main_model, np.array([35, 1, 6, 8, 5, 5, 5]))
+    print_testcase(env, main_model, np.array([33, 4, 0, 6, 7, 4, 6]))
+    print_testcase(env, main_model, np.array([31, 10, 3, 3, 8, 2, 9]))
+
+    # prbs req > 10
+    # print_testcase(env, main_model, np.array([37, 25, 14, 3, 5, 6, 2]))
+    # print_testcase(env, main_model, np.array([45, 0, 29, 17, 2, 1, 2]))
+    # print_testcase(env, main_model, np.array([41, 21, 16, 20, 5, 1, 3]))
+    # print_testcase(env, main_model, np.array([11, 20, 13, 13, 18, 12, 9]))
 
 
 def test(env, main_model):
     # Testing
     # initializing the environment
     env.time = 0
-    env.state = np.array([prbs_inf_init, env.prbs_req_data[env.time], env.prbs_req_data[env.time],
-                               env.prbs_req_data[env.time], min_curr_prbs, min_curr_prbs, min_curr_prbs])
+    env.state = np.array([prbs_inf_init, env.prbs_req_s1[env.time], env.prbs_req_s2[env.time],
+                           env.prbs_req_s3[env.time], 0, 0, 0])
 
-    print("pRBs_inf \t req_s1 \t req_s2 \t req_s3 \t pRBs_s1 \t pRBs_s2 \t pRBs_s3 \t A1 \t A2 \t A3")
-    for i in range(0, env.episode_length):  # upper bound is exclusive
+    num_impossible_actions = 0
+    num_under_allocations = 0
+    num_over_allocations = 0
+    total_under_allocated = 0
+    total_over_allocated = 0
+    cumulative_reward = 0
+    s1_record = np.zeros(testing_iterations)
+    s2_record = np.zeros(testing_iterations)
+    s3_record = np.zeros(testing_iterations)
+
+    # print("State \t \t Action \t \t Q-value")
+    for i in range(0, testing_iterations):  # upper bound is exclusive
+        state = tuple(env.state)
         # select action (exploit)
-        predicted = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
-        # print(np.reshape(np.round(predicted, 3), (5, 5)))
-        action = env.action_space_MI[np.argmax(predicted)]
-        print("{} \t \t \t {} \t \t {} \t \t {} \t \t {} \t \t {} \t \t {} \t \t {}  \t {}  \t {}"
-              .format(env.state[0], env.state[1], env.state[2], env.state[3], env.state[4],
-                      env.state[5], env.state[6], action[0], action[1], action[2]))
+        predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()  # Exploit learned values
+        action = env.action_space_MI[np.argmax(predictions)]
+        q_value = np.max(predictions)
+        # print("State: ", env.state, "\t Action: ", action, "\t Q-value: ", q_value)
+        # print(' '.join(map(str, env.state)), ' '.join(map(str, action)), q_value)
         # move 1 step forward
-        next_state, reward, done, truncated, info = env.step(action)
+        next_state, reward, done, truncated, info = env.step(action, True)
 
-    # Test other inputs and print out the results
-    # test_other_inputs(env, main_model)
+        # Test metrics
+        cumulative_reward += reward
+        s1_record[i] = next_state[4]
+        s2_record[i] = next_state[5]
+        s3_record[i] = next_state[6]
+        for slice in range(1, num_slices + 1):
+            prev_req = state[slice]
+            allocated = next_state[slice + num_slices]
+            prev_allocated = state[slice + num_slices]
+            if prev_allocated + action[slice-1] < 0:  # impossible action
+                num_impossible_actions += 1
+                num_under_allocations += 1
+                total_under_allocated += prev_req - (prev_allocated + action[slice-1])
+            elif allocated < prev_req:  # under-allocation
+                num_under_allocations += 1
+                total_under_allocated += prev_req - allocated
+            elif allocated > prev_req:  # over-allocation
+                num_over_allocations += 1
+                total_over_allocated += allocated - prev_req
+
+        state = next_state
+
+    print("Number of Impossible Actions: ", num_impossible_actions)
+    print("Number of Under-allocations: ", num_under_allocations)
+    print("Number of Over-allocations: ", num_over_allocations)
+    print("Total amount Under-allocated: ", total_under_allocated)
+    print("Total amount Over-allocated: ", total_over_allocated)
+    print("Cumulative Reward: ", cumulative_reward)
+
+    if show_plots:
+        plot.plot(np.arange(0, testing_iterations), env.prbs_req_s1[:testing_iterations], marker='o', label='Required')
+        plot.plot(np.arange(0, testing_iterations), s1_record, marker='x', label='Allocated')
+        plot.title('Slice 1')
+        plot.grid(True)
+        plot.xticks(np.arange(0, testing_iterations, 1))
+        plot.savefig('Slice_1.jpg')
+        plot.show()
+
+        plot.plot(np.arange(0, testing_iterations), env.prbs_req_s2[:testing_iterations], marker='o', label='Required')
+        plot.plot(np.arange(0, testing_iterations), s2_record, marker='x', label='Allocated')
+        plot.title('Slice 2')
+        plot.grid(True)
+        plot.xticks(np.arange(0, testing_iterations, 1))
+        plot.savefig('Slice_2.jpg')
+        plot.show()
+
+        plot.plot(np.arange(0, testing_iterations), env.prbs_req_s3[:testing_iterations], marker='o', label='Required')
+        plot.plot(np.arange(0, testing_iterations), s3_record, marker='x', label='Allocated')
+        plot.title('Slice 3')
+        plot.grid(True)
+        plot.xticks(np.arange(0, testing_iterations, 1))
+        plot.savefig('Slice_3.jpg')
+        plot.show()
+
 
 def main():
 
@@ -256,44 +345,22 @@ def main():
     agent = RicAgent()
 
     # main_model = agent.build_net(env.state.shape, len(env.action_space))
-    main_model = load_model("test_23_3.h5")
+    main_model = load_model("test_25_3.h5")
     replay_memory = deque(maxlen=50_000)
 
-    # Hyper-parameters
-    epsilon = 0.7  # want to explore all the time
-
     # Training
-    for i in range(1, 5000):
+    for i in range(1, num_iterations):
         print(i)
-        # select action (explore vs exploit)
-        # if random.uniform(0, 1) < epsilon:
-        #     action = env.action_space.sample().index[0]  # Explore action space
-        #     action_index = env.action_space_MI.get_loc(action)
-        # else:
-        #     predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
-        #     action = env.action_space_MI[np.argmax(predictions)]
-        #     action_index = env.action_space_MI.get_loc(action)
+        # epsilon = initial_epsilon - (i / num_iterations) * (initial_epsilon - final_epsilon)
 
-        if random.uniform(0, 1) < 0.5:
-            env.state = np.array([8, 2, 2, 2, 0, 0, 0])
-            state = tuple(env.state)
-            if random.uniform(0, 1) < epsilon:
-                action = env.action_space.sample().index[0]  # Explore action space
-                action_index = env.action_space_MI.get_loc(action)
-            else:
-                predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
-                action = env.action_space_MI[np.argmax(predictions)]
-                action_index = env.action_space_MI.get_loc(action)
+        # select action (explore vs exploit)
+        if random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample().index[0]  # Explore action space
+            action_index = env.action_space_MI.get_loc(action)
         else:
-            env.state = np.array([4, 0, 0, 0, 2, 2, 2])
-            state = tuple(env.state)
-            if random.uniform(0, 1) < epsilon:
-                action = env.action_space.sample().index[0]  # Explore action space
-                action_index = env.action_space_MI.get_loc(action)
-            else:
-                predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
-                action = env.action_space_MI[np.argmax(predictions)]
-                action_index = env.action_space_MI.get_loc(action)
+            predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
+            action = env.action_space_MI[np.argmax(predictions)]
+            action_index = env.action_space_MI.get_loc(action)
 
         # move 1 step forward
         next_state, reward, done, truncated, info = env.step(action)
@@ -305,10 +372,10 @@ def main():
         state = next_state
 
     # Save model
-    main_model.save("test_23_3.h5")
+    # main_model.save("test_26_1.h5")
     # Testing
-    # test(env, main_model)
-    test_other_inputs(env, main_model)
+    test(env, main_model)
+    # test_other_inputs(env, main_model)
 
 
 if __name__ == "__main__":
