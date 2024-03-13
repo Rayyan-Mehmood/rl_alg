318ab293b3e0700c5eb56ff76415b6005e36bd77
Rayyan-Mehmood <rayyan.mehmood@gmail.com>
2024-03-13 20:15:06 +0000

    Failed to work DQL on smallest possible state space

--- dql_alg.py
+++ dql_alg.py
@@ -8,23 +8,26 @@ from keras.models import load_model
 from collections import deque
 
 num_slices = 3
-prbs_inf_init = 50
+prbs_inf_init = 3
 max_prbs_inf = prbs_inf_init
-max_req_prbs = 10  # max total prbs a slice can require
+max_req_prbs = 1  # max total prbs a slice can require
 min_req_prbs = 0
-max_curr_prbs = 35
+max_curr_prbs = 1
 min_curr_prbs = 0
-max_allocate = 35
-min_allocate = -35
-num_iterations = 5000
-testing_iterations = 50
-epsilon = 0.5
-impossible_action_reward = -150
+max_allocate = 1
+min_allocate = -1
+num_episodes = 100
+episode_length = 10
+testing_iterations = 10
+initial_epsilon = 0.99
+final_epsilon = 0.01
+impossible_action_reward = -5
 show_plots = True
-old_model = "test_28_7.h5"
-new_model = "test_28_7.h5"
-# initial_epsilon = 0.9
-# final_epsilon = 0.4
+old_main_model = "test_29_1_m.h5"
+new_main_model = "test_29_1_m.h5"
+old_target_model = "test_29_1_t.h5"
+new_target_model = "test_29_1_t.h5"
+
 
 class RicEnv:
     def __init__(self):
@@ -47,7 +50,11 @@ class RicEnv:
         # sine_time_range = np.arange(0, 10, 0.5) # generating manually
         # self.prbs_req_data = np.rint((max_req_prbs / 2) * np.sin(sine_time_range) + max_req_prbs / 2)  # already changed to cosine
         self.time = 0
-        self.episode_length = 20
+        self.state = np.array([prbs_inf_init, random.randint(0, max_req_prbs), random.randint(0, max_req_prbs),
+                               random.randint(0, max_req_prbs), min_curr_prbs, min_curr_prbs, min_curr_prbs])
+
+    def init_state(self):
+        self.time = 0
         self.state = np.array([prbs_inf_init, random.randint(0, max_req_prbs), random.randint(0, max_req_prbs),
                                random.randint(0, max_req_prbs), min_curr_prbs, min_curr_prbs, min_curr_prbs])
 
@@ -57,11 +64,11 @@ class RicEnv:
         # r = number of pRBs slice requires
         # a = number of pRBs slice currently has
         if a == r:
-            individual_reward = 120
+            individual_reward = 10
         elif a > r:
-            individual_reward = 100 - (10 * (a - r))
+            individual_reward = 5
         else:
-            individual_reward = -100 - (10 * (r - abs(a)))
+            individual_reward = -5
 
         return individual_reward
 
@@ -80,9 +87,9 @@ class RicEnv:
         new_curr_prbs_s1 = curr_prbs_s1 + action[0]
         new_curr_prbs_s2 = curr_prbs_s2 + action[1]
         new_curr_prbs_s3 = curr_prbs_s3 + action[2]
-        self.time = self.time + 1  # update time
-        if self.time >= self.episode_length and not testing:
-            self.time = 0  # reset time
+        # update the time
+        self.time = self.time + 1
+        # update the state
         if not testing:
             self.state = np.array([new_prbs_inf, random.randint(0, max_req_prbs), random.randint(0, max_req_prbs),
                                random.randint(0, max_req_prbs), new_curr_prbs_s1, new_curr_prbs_s2, new_curr_prbs_s3])
@@ -142,14 +149,29 @@ class RicEnv:
             prev_state[3] = round(prev_state[6] + oa3)
 
         # Calculate the individual reward for each slice and then sum the rewards
-        total_reward = 0
-        individual_reward = 0
+        impossible = False
+        under = False
+        over = False
         for slice in range(1, num_slices + 1):
-            individual_reward = self.calc_individual_reward(prev_state[slice], self.state[slice + num_slices])
-            total_reward = total_reward + individual_reward
-        reward = total_reward
+            a = self.state[slice + num_slices]
+            r = prev_state[slice]
+            if a < 0:
+                impossible = True
+            elif a < r:
+                under = True
+            elif a > r:
+                over = True
 
-        # Impossible actions (which lead to prbs_inf being negative for e.g.) are punished heavily
+        if impossible:
+            reward = -5
+        elif under:
+            reward = -5
+        elif over:
+            reward = 5
+        else:
+            reward = 10
+
+        # Bounding the states...
         if new_prbs_inf < 0:
             reward = impossible_action_reward
             if bounded: self.state[0] = 0  # set prbs_inf to zero
@@ -157,54 +179,46 @@ class RicEnv:
             if bounded: self.state[0] = max_prbs_inf
 
         if new_curr_prbs_s1 < 0:
-            reward = impossible_action_reward
             if bounded: self.state[4] = 0
         elif new_curr_prbs_s1 > max_curr_prbs:
             if bounded: self.state[4] = max_curr_prbs
 
         if new_curr_prbs_s2 < 0:
-            reward = impossible_action_reward
             if bounded: self.state[5] = 0
         elif new_curr_prbs_s2 > max_curr_prbs:
             if bounded: self.state[5] = max_curr_prbs
 
         if new_curr_prbs_s3 < 0:
-            reward = impossible_action_reward
             if bounded: self.state[6] = 0
         elif new_curr_prbs_s3 > max_curr_prbs:
             if bounded: self.state[6] = max_curr_prbs
 
-        if self.time == 0:
-            self.state[0] = prbs_inf_init
-            self.state[4] = min_curr_prbs
-            self.state[5] = min_curr_prbs
-            self.state[6] = min_curr_prbs
-
         info = {}
-        done = False
+        if self.time >= episode_length:
+            done = True
+        else:
+            done = False
         truncated = False
         return tuple(self.state), reward, done, truncated, info
 
 class RicAgent:
     def build_net(self, state_shape, num_actions):
-        """ The agent maps X-states to Y-actions
-        e.g. The neural network output is [.1, .7, .1, .3]
-        The highest value 0.7 is the Q-Value.
-        The index of the highest action (0.7) is action #1.
-        """
         learning_rate = 0.01
         optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
 
         model = keras.Sequential()
         model.add(keras.layers.Input(shape=state_shape))
-        model.add(keras.layers.Dense(64, activation='relu'))
-        model.add(keras.layers.Dense(32, activation='relu'))
+        model.add(keras.layers.Dense(24, activation='relu'))
+        model.add(keras.layers.Dense(24, activation='relu'))
         model.add(keras.layers.Dense(num_actions, activation='linear'))
-        model.compile(optimizer=optimizer, loss='mse')
+        model.compile(loss='mse', optimizer=optimizer, metrics=['accuracy'])
         # model.summary()
         return model
 
-    def train(self, replay_memory, model, done):
+    def train(self, replay_memory, model, target_model):
+        learning_rate = 0.7
+        discount_factor = 0.3
+
         MIN_REPLAY_SIZE = 500
         if len(replay_memory) < MIN_REPLAY_SIZE:
             return
@@ -213,42 +227,71 @@ class RicAgent:
         mini_batch = random.sample(replay_memory, batch_size)
 
         # Collect observations from the mini-batch
-        X = np.array([transition[0] for transition in mini_batch])
-
+        current_states = np.array([transition[0] for transition in mini_batch])
         # Predict Q-values for the entire batch
-        current_qs_list = model.predict(X)
+        current_qs_list = model.predict(current_states)
+        next_states = np.array([transition[3] for transition in mini_batch])
+        next_qs_list = target_model.predict(next_states)
 
+        X = []
         Y = []
         for index, (observation, action, reward, new_observation, done) in enumerate(mini_batch):
+            max_new_q = reward + discount_factor * np.max(next_qs_list[index])
             current_qs = current_qs_list[index]
-            current_qs[action] = reward
+            current_qs[action] = (1 - learning_rate) * current_qs[action] + learning_rate * max_new_q
 
+            X.append(observation)
             Y.append(current_qs)
 
-        model.fit(X, np.array(Y), batch_size=batch_size, verbose=0, shuffle=True)
+        model.fit(np.array(X), np.array(Y), batch_size=batch_size, verbose=0, shuffle=True)
 
 
-def test_other_inputs(env, main_model):
-    def print_testcase(env, main_model, state):
+def test_other_inputs(env, main_model, target_model):
+    def print_testcase(env, main_model, state, target_model):
         env.state = state
         print("State: ", env.state)
         predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()
         action = env.action_space_MI[np.argmax(predictions)]
         q_value = np.max(predictions)
         print("Action: ", action)
-        print("Q-value: ", q_value)
-        print(predictions[env.action_space_MI.get_loc((2, 2, 2))])
+        print("Main Model Q-value: ", q_value)
+        # predictions = target_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten()
+        # q_value = np.max(predictions)
+        # print("Target Model Q-value: ", q_value)
+        # print(predictions[env.action_space_MI.get_loc((2, 2, 2))])
 
     # Testing other inputs
     print("TESTING OTHER INPUTS")
 
-    print_testcase(env, main_model, np.array([50, 2, 2, 2, 0, 0, 0]))
-    print_testcase(env, main_model, np.array([40, 0, 0, 0, 2, 6, 2]))
-    print_testcase(env, main_model, np.array([48, 0, 9, 7, 0, 0, 2]))
-    print_testcase(env, main_model, np.array([40, 1, 6, 2, 6, 2, 2]))
-    print_testcase(env, main_model, np.array([35, 1, 6, 8, 5, 5, 5]))
-    print_testcase(env, main_model, np.array([33, 4, 0, 6, 7, 4, 6]))
-    print_testcase(env, main_model, np.array([31, 10, 3, 3, 8, 2, 9]))
+    # print_testcase(env, main_model, np.array([50, 2, 2, 2, 0, 0, 0]))
+    # print_testcase(env, main_model, np.array([40, 0, 0, 0, 2, 6, 2]))
+    # print_testcase(env, main_model, np.array([48, 0, 9, 7, 0, 0, 2]))
+    # print_testcase(env, main_model, np.array([40, 1, 6, 2, 6, 2, 2]))
+    # print_testcase(env, main_model, np.array([35, 1, 6, 8, 5, 5, 5]))
+    # print_testcase(env, main_model, np.array([33, 4, 0, 6, 7, 4, 6]))
+    # print_testcase(env, main_model, np.array([31, 10, 3, 3, 8, 2, 9]))
+
+    # print_testcase(env, main_model, np.array([50, 2, 2, 2, 0, 0, 0]))
+    # print_testcase(env, main_model, np.array([50, 1, 1, 1, 0, 0, 0]))
+    # print_testcase(env, main_model, np.array([50, 0, 0, 0, 0, 0, 0]))
+    # print_testcase(env, main_model, np.array([50, 1, 0, 1, 0, 0, 0]))
+    # print_testcase(env, main_model, np.array([50, 1, 1, 1, 1, 1, 1]))
+    # print_testcase(env, main_model, np.array([50, 1, 1, 1, 2, 0, 0]))
+    # print_testcase(env, main_model, np.array([50, 1, 1, 1, 0, 2, 0]))
+    # print_testcase(env, main_model, np.array([50, 1, 1, 1, 0, 1, 1]))
+    # print_testcase(env, main_model, np.array([50, 2, 1, 2, 0, 0, 0]))
+    # print_testcase(env, main_model, np.array([50, 1, 2, 0, 2, 0, 0]))
+
+    print_testcase(env, main_model, np.array([3, 0, 0, 0, 0, 0, 0]), target_model)
+    print_testcase(env, main_model, np.array([3, 1, 1, 1, 0, 0, 0]), target_model)
+    print_testcase(env, main_model, np.array([2, 0, 0, 0, 1, 0, 0]), target_model)
+    print_testcase(env, main_model, np.array([2, 1, 1, 0, 0, 1, 0]), target_model)
+    print_testcase(env, main_model, np.array([2, 0, 0, 1, 0, 1, 0]), target_model)
+    print_testcase(env, main_model, np.array([1, 1, 0, 0, 1, 0, 1]), target_model)
+    print_testcase(env, main_model, np.array([1, 1, 1, 1, 1, 0, 1]), target_model)
+    print_testcase(env, main_model, np.array([1, 0, 0, 0, 0, 1, 1]), target_model)
+    print_testcase(env, main_model, np.array([0, 1, 1, 1, 1, 1, 1]), target_model)
+    print_testcase(env, main_model, np.array([0, 0, 0, 0, 1, 1, 1]), target_model)
 
 
     # prbs req > 10
@@ -286,7 +329,7 @@ def test(env, main_model):
         print(i, ": State: ", env.state, "\t Action: ", action, "\t Q-value: ", q_value)
         # print(' '.join(map(str, env.state)), ' '.join(map(str, action)), q_value)
         # move 1 step forward
-        next_state, reward, done, truncated, info = env.step(action, True, False)
+        next_state, reward, done, truncated, info = env.step(action, True, True)
 
         # Test metrics
         cumulative_reward += reward
@@ -361,42 +404,57 @@ def main():
     state = tuple(env.state)
     agent = RicAgent()
 
-    # main_model = agent.build_net(env.state.shape, len(env.action_space))
-    main_model = load_model(old_model)
-    # learning_rate = 0.5
-    # optimizer = keras.optimizers.Nadam(learning_rate=learning_rate)
-    # main_model.compile(loss='mse', optimizer=optimizer, metrics=['accuracy'])
+    main_model = agent.build_net(env.state.shape, len(env.action_space))
+    # main_model = load_model(old_main_model)
+
+    target_model = agent.build_net(env.state.shape, len(env.action_space))
+    # target_model = load_model(old_target_model)
+    target_model.set_weights(main_model.get_weights())
+
     replay_memory = deque(maxlen=50_000)
+    epsilon = initial_epsilon
 
     # Training
-    for i in range(1, num_iterations):
-        print(i)
-        # epsilon = initial_epsilon - (i / num_iterations) * (initial_epsilon - final_epsilon)
-        # select action (explore vs exploit)
-        if random.uniform(0, 1) < epsilon:
-            action = env.action_space.sample().index[0]
-            # action = (random.randint(-5, 5), random.randint(-5, 5), random.randint(-5, 5))
-            action_index = env.action_space_MI.get_loc(action)
-        else:
-            predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
-            action = env.action_space_MI[np.argmax(predictions)]
-            action_index = env.action_space_MI.get_loc(action)
+    for episode in range(1, num_episodes + 1):
 
-        # move 1 step forward
-        next_state, reward, done, truncated, info = env.step(action, False, True)
-        replay_memory.append([state, action_index, reward, next_state, done])
-        # Train main network
-        if i % 10 == 0:
-            agent.train(replay_memory, main_model, done)
-        # update state
-        state = next_state
+        env.init_state()
+        state = tuple(env.state)
+
+        for i in range(1, episode_length+1):
+            print("Episode: ", episode, "Timestep: ", i)
+            # select action (explore vs exploit)
+            if random.uniform(0, 1) < epsilon:
+                action = env.action_space.sample().index[0]
+                action_index = env.action_space_MI.get_loc(action)
+            else:
+                predictions = main_model.predict(env.state.reshape([1, env.state.shape[0]])).flatten() # Exploit learned values
+                action = env.action_space_MI[np.argmax(predictions)]
+                action_index = env.action_space_MI.get_loc(action)
+
+            # move 1 step forward
+            next_state, reward, done, truncated, info = env.step(action, False, False)
+            replay_memory.append([state, action_index, reward, next_state, done])
+            # update state
+            state = next_state
+
+        # after each episode
+        epsilon = initial_epsilon - (episode / num_episodes) * (initial_epsilon - final_epsilon)
+        # Train main network every n episodes
+        if episode % 10 == 0:
+            agent.train(replay_memory, main_model, target_model)
+
+        # Update target network weights every n episodes
+        if episode % 30 == 0:
+            target_model.set_weights(main_model.get_weights())
+
+
 
     # Save model
-    main_model.save(new_model)
+    main_model.save(new_main_model)
+    target_model.save(new_target_model)
     # Testing
     # test(env, main_model)
-    test_other_inputs(env, main_model)
-
+    test_other_inputs(env, main_model, target_model)
 
 if __name__ == "__main__":
     main()
